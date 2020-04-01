882dfe71a2a8cc126f7a29ccbfb4a152f463fe68
lestrix <lestrix@gmx.de>
2020-04-01 02:07:20 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,39 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
+import SEO from "../components/seo"
+// import { FaGulp } from "react-icons/fa"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+// class Question extends React.Component {
+//   render() {
+//     return (
+//       <h3>
+//         {" "}
+//         Lets go for a <FaGulp />?{" "}
+//       </h3>
+//     )
+//   }
+// }
 
+// sss
+// import { FaBeer } from "react-icons/fa"
+
+// class Question extends React.Component {
+//   render() {
+//     return (
+//       <h3>
+//         {" "}
+//         Lets go for a <FaBeer />?{" "}
+//       </h3>
+//     )
+//   }
+// }
+// sss
 export default IndexPage
